d367abd565db79bb918d8d9ea8ae3b33198e01a2
LordBaryhobal <47377497+LordBaryhobal@users.noreply.github.com>
2023-11-29 09:21:48 +0100

    changed match-case to array of colors

--- TetrisPiecesColor.scala
+++ TetrisPiecesColor.scala
@@ -2,32 +2,18 @@ package ByteToTetrisPieces
 import java.awt.Color
 
 object TetrisPiecesColor {
+  private val COLORS: Array[Color] = Array(
+    new Color(0, 0, 0),
+    new Color(0, 0, 255),
+    new Color(0, 255, 0),
+    new Color(0, 255, 255),
+    new Color(255, 0, 0),
+    new Color(255, 0, 255),
+    new Color(255, 255, 0),
+    new Color(255, 255, 255)
+  )
   def getColorFromByte(byte: Byte): Color = {
     val bite0To3: Byte = (byte & 0xF8).toByte
-    bite0To3 match {
-      case 0 => return new Color(0, 0, 0)
-
-      // 001
-      case 1 => return new Color(0, 0, 255)
-
-      // 010
-      case 2 => return new Color(0, 255, 0)
-
-      // 011
-      case 3 => return new Color(0, 255, 255)
-
-      // 100
-      case 4 => return new Color(255, 0, 0)
-
-      // 101
-      case 5 => return new Color(255, 0, 255)
-
-      // 110
-      case 6 => return new Color(255, 255, 0)
-
-      // 111
-      case 7 => return new Color(255, 255, 255)
-    }
+    COLORS(bite0To3)
   }
-
 }
