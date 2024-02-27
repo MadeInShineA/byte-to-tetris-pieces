bb26f08d90f4e578c469e95dbef2930a1ee6963d
MadeInShineA <olivier.amacker@netplus.ch>
2024-02-27 11:21:45 +0100

    2nd Semester Update

--- Runner.scala
+++ Runner.scala
@@ -2,7 +2,7 @@ package ByteToTetrisPieces
 
 object Runner extends App{
 
-  val s1: String = "Hello World! En Pièces Tetris"
+  val s1: String = "Coucou Olive"
   val s1Bytes = s1.getBytes
   for (b: Byte <- s1Bytes) {
     println(TetrisPiece.byteToTetrisPieces(b))
